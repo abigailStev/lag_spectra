c78a3beda0e5dce7dbad11be482cc3e5dc2fdfa3
Abigail Stevens <A.L.Stevens@uva.nl>
2015-11-12 14:12:50 +0100

    Simple script to overplot multiple lag-energy spectra (simulations, data, etc).

---
+++ overplot_lag-energy.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env
+"""
+Plots multiple lag-energy spectra (from different observations, simulations,
+etc.) on the same plot.
+
+Change lag_dir and sim_dir to suit your machine and setup.
+
+Example call: python overplot_lag-energy.py cygx1
+"""
+import argparse
+import subprocess
+import numpy as np
+from astropy.io import fits
+import os.path
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
+from matplotlib.ticker import MultipleLocator
+from matplotlib.ticker import ScalarFormatter
+
+import tools  ## in https://github.com/abigailStev/whizzy_scripts
+# import get_lags as lags
+
+__author__ = 'Abigail Stevens <A.L.Stevens at uva.nl>'
+__year__ = '2015'
+
+HOME_DIR = os.path.expanduser("~")
+
+PLOT_EXT = "eps"
+LO_FREQ = 4.0
+UP_FREQ = 7.0
+LO_ENERGY = 3.0
+UP_ENERGY = 20.0
+
+DETCHANS = 64
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(usage="python overplot_lag-energy.py "\
+            "prefix", description=__doc__)
+
+    parser.add_argument('prefix', help="Identifying prefix of data (object "\
+            "nickname or data ID).")
+    args = parser.parse_args()
+    prefix = args.prefix
+
+    lag_dir = HOME_DIR + "/Dropbox/Research/lags/out_lags/" + prefix
+    sim_dir = HOME_DIR + "/Dropbox/Research/simulate/out_sim/" + prefix
+
+    # ## Get necessary information and data from the input file
+    # freq, cs_avg, power_ci, power_ref, dt, n_bins, DETCHANS, num_seconds, \
+    #         num_seg, mean_rate_ci, mean_rate_ref, evt_list = lags.get_inputs(in_file)
+    #
+    # #########################################
+    # ## Computing the lags of the fundamental
+    # #########################################
+    #
+    # f_phase_1, f_err_phase_1, f_tlag_1, f_err_tlag_1, e_phase_1, e_err_phase_1, \
+    #         e_tlag_1, e_err_tlag_1 = lags.compute_lags(freq, cs_avg, power_ci, \
+    #         power_ref, dt, n_bins, DETCHANS, num_seconds, num_seg, mean_rate_ci, \
+    #         mean_rate_ref, lo_freq, up_freq, lo_energy, up_energy)
+    #
+    # #########################################
+    # ## Writing the output
+    # #########################################
+    #
+    # # lags.fits_out(out_file, in_file, evt_list, dt, n_bins, num_seg, DETCHANS,
+    # #          lo_freq, up_freq, lo_energy, up_energy, mean_rate_ci,
+    # #          mean_rate_ref, freq, f_phase_1, f_err_phase_1, f_tlag_1, f_err_tlag_1,
+    # #          e_phase_1, e_err_phase_1, e_tlag_1, e_err_tlag_1)
+    #
+    #
+    # ###########################################
+    # ## Computing the lags of only the harmonic
+    # ###########################################
+    #
+    #
+    # in_file = sim_dir + "/FAKE-" + prefix + "_150722_nopoiss_cs.fits"
+    #
+    # ## Get necessary information and data from the input file
+    # freq, cs_avg, power_ci, power_ref, dt, n_bins, DETCHANS, num_seconds, \
+    #         num_seg, mean_rate_ci, mean_rate_ref, evt_list = lags.get_inputs(in_file)
+    #
+    # f_phase_2, f_err_phase_2, f_tlag_2, f_err_tlag_2, e_phase_2, e_err_phase_2, \
+    #         e_tlag_2, e_err_tlag_2 = lags.compute_lags(freq, cs_avg, power_ci, \
+    #         power_ref, dt, n_bins, DETCHANS, num_seconds, num_seg, mean_rate_ci, \
+    #         mean_rate_ref, lo_freq, up_freq, lo_energy, up_energy)
+    #
+    # ###########################################################
+    # ## Computing the lags of both the fundamental and harmonic
+    # ###########################################################
+    #
+    # ## Get necessary information and data from the input file
+    # freq, cs_avg, power_ci, power_ref, dt, n_bins, DETCHANS, num_seconds, \
+    #         num_seg, mean_rate_ci, mean_rate_ref, evt_list = lags.get_inputs(in_file)
+    #
+    # f_phase_3, f_err_phase_3, f_tlag_3, f_err_tlag_3, e_phase_3, e_err_phase_3, \
+    #         e_tlag_3, e_err_tlag_3 = lags.compute_lags(freq, cs_avg, power_ci, \
+    #         power_ref, dt, n_bins, DETCHANS, num_seconds, num_seg, mean_rate_ci, \
+    #         mean_rate_ref, lo_freq, up_freq, lo_energy, up_energy)
+
+    in_file_list = [lag_dir + "/" + prefix + "_150902_t64_64sec_adj_lag.fits",
+                    # sim_dir + "/FAKE-" + prefix + "_150902_1BB-FS-G-fzs_lag.fits",
+                    # sim_dir + "/FAKE-" + prefix + "_150902_1BB-FS-G-E-fzs_lag.fits",
+                    # sim_dir + "/FAKE-" + prefix + "_150902_1BB-FS-G-NE-fzs_lag.fits",
+                    sim_dir + "/FAKE-" + prefix + "_150902_1BB-FS-G-Tin-fzs-fzNbb-wMCMC_lag.fits"]
+    colours=["black",
+            # "green",
+             # "orange",
+             # 'magenta',
+             "blue"
+             ]
+    markers = ['o',
+               'd'
+               # '*',
+               # # '.',
+               # 'x'
+               ]
+    # labels = [r"PL norm & index, $\chi^{2}=165.8$",
+    #           r"+ iron line energy, $\chi^{2}=144.4$",
+    #           # r"+ iron line norm, $\chi^{2}=78.0$",
+    #           r"+ BB temp, $\chi^{2}=37.8$",
+    #           "Data"]
+    labels = ["Data", "Simulation"]
+
+    plot_root = sim_dir + "/FAKE-" + prefix + "_150902_wdata_w1BB"
+
+    #######################
+    ## Setting up the plot
+    #######################
+
+    font_prop = font_manager.FontProperties(size=20)
+    energies = np.loadtxt(HOME_DIR + "/Reduced_data/" + prefix +
+                          "/energies.txt")
+    energy_list = [np.mean([x, y]) for x,y in tools.pairwise(energies)]
+    energy_err = [np.abs(a-b) for (a,b) in zip(energy_list, energies[0:-1])]
+
+    plot_file = plot_root + "_lag-energy." + plot_ext
+    print "Lag-energy spectrum: %s" % plot_file
+
+    e_chans = np.arange(0, DETCHANS)
+
+
+    ###################
+    ## Making the plot
+    ###################
+
+    fig, ax = plt.subplots(1, 1, figsize=(10,7.5), dpi=300, tight_layout=True)
+
+    ax.hlines(0.0, 3, 21, linestyle='dashed', lw=2, color='gray')
+
+    ## Deleting the values at energy channel 10 for RXTE PCA event-mode data
+    if DETCHANS == 64:
+        e_chans = np.delete(e_chans, 10)
+        energy_list = np.delete(energy_list, 10)
+        energy_err = np.delete(energy_err, 10)
+    i = 0
+
+    for in_file in in_file_list:
+        try:
+            fits_hdu = fits.open(in_file)
+        except IOError:
+            print "\tERROR: File does not exist: %s" % in_file
+            exit()
+
+        # print fits_hdu.info()
+
+        tlag = fits_hdu[2].data.field('TIME_LAG')
+
+        tlag_err = fits_hdu[2].data.field('TIME_LAG_ERR')
+
+        ## Deleting the values at energy channel 10 for RXTE PCA event-mode data
+        if DETCHANS == 64:
+            tlag = np.delete(tlag, 10)
+            tlag_err = np.delete(tlag_err, 10)
+
+            ax.errorbar(energy_list[2:26], tlag[2:26], xerr=energy_err[2:26],
+                    yerr=tlag_err[2:26], ls='none', marker=markers[i], ms=10,
+                    mew=2, mec=colours[i], mfc=colours[i], ecolor=colours[i],
+                    elinewidth=2, capsize=0, label=labels[i])
+        else:
+            ax.errorbar(energy_list, tlag, xerr=energy_err,
+                    yerr=tlag_err, ls='none', marker=markers[i], ms=10, mew=2,
+                    mec=colours[i], mfc=colours[i], ecolor=colours[i],
+                    elinewidth=2, capsize=0, label=labels[i])
+        i += 1
+
+    # ax.plot([0,DETCHANS],[0,0], lw=1.5, ls='dashed', c='black')
+    # 	ax.plot([0,DETCHANS],[np.pi,np.pi], lw=1.5, ls='dashed', c='black')
+    # 	ax.plot([0,DETCHANS],[-np.pi,-np.pi], lw=1.5, ls='dashed', c='black')
+    # 	ax.errorbar(e_chans, phase, yerr=err_phase, lw=3, c='red', \
+    # 		ls="steps-mid", elinewidth=2, capsize=2)
+
+    ax.set_xlabel('Energy (keV)', fontproperties=font_prop)
+    ax.set_xlim(3, 21)
+    # ax.set_xlim(0.3, 10)
+    # ax.set_ylim(-0.009, 0.016)
+    ax.set_xscale('log')
+    x_maj_loc = [5, 10, 20]
+    # y_maj_loc = [-0.005, 0, 0.005, 0.01, 0.015]
+    ax.set_xticks(x_maj_loc)
+    # ax.set_yticks(y_maj_loc)
+    xLocator = MultipleLocator(1)  ## loc of minor ticks on x-axis
+    yLocator = MultipleLocator(0.001)  ## loc of minor ticks on y-axis
+    ax.xaxis.set_minor_locator(xLocator)
+    ax.yaxis.set_minor_locator(yLocator)
+    ax.xaxis.set_major_formatter(ScalarFormatter())
+
+    ax.set_ylabel('Time lag (s)', fontproperties=font_prop)
+    # ax.set_ylabel('Phase lag (radians)', fontproperties=font_prop)
+    ax.set_ylim(-0.01, 0.017)
+    # ax.set_ylim(-0.4, 0.5)
+    ax.tick_params(axis='x', labelsize=18)
+    ax.tick_params(axis='y', labelsize=18)
+    # ax.set_title("Lag-energy spectrum", fontproperties=font_prop)
+
+    ## The following legend code was found on stack overflow I think
+    legend = ax.legend(loc='upper left')
+    for label in legend.get_texts():
+        label.set_fontsize(18)
+    for label in legend.get_lines():
+        label.set_linewidth(2)  # the legend line width
+
+    plt.savefig(plot_file)
+    # 	plt.show()
+    plt.close()
+    subprocess.call(['open', plot_file])
